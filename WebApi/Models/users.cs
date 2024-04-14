751e40e963c2da6a54150a6e6267e786f85f505c
Daniel <danieljude1992@gmail.com>
2024-04-14 21:31:50 +0100

    flutter app using .Net backend

---
+++ WebApi/Models/users.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Models
+{
+    public class Users
+    {
+        [Key]
+        public int userid { get; set; }
+
+        [Required]
+        public string name { get; set; } = "";
+
+        public string address { get; set; } = "";
+    }
+}
